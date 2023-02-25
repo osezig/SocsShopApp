3daa8e60f037a06e189f5ce2f0a51acc854a5378
osezig <113824525+osezig@users.noreply.github.com>
2023-02-25 08:29:48 +0500

    Курсовая работа №3

---
+++ src/main/java/com/iagfarov/socsshopapp/SocsShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.iagfarov.socsshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocsShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocsShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iagfarov/socsshopapp/SocsShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagfarov.socsshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocsShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
