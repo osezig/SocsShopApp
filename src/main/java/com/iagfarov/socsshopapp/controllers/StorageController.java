39cf3de5a853bf46a2bf208c05143349d7cf371a
osezig <113824525+osezig@users.noreply.github.com>
2023-02-27 22:21:55 +0500

    Курсовая работа №3

--- src/main/java/com/iagfarov/socsshopapp/controllers/StorageController.java
+++ src/main/java/com/iagfarov/socsshopapp/controllers/StorageController.java
@@ -4,35 +4,44 @@ import com.iagfarov.socsshopapp.enums.Size;
 import com.iagfarov.socsshopapp.exceptions.QuantityException;
 import com.iagfarov.socsshopapp.models.Socks;
 import com.iagfarov.socsshopapp.services.StorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/storage")
+@RequiredArgsConstructor
+@RequestMapping("/socks")
+@Tag(name = "Склад носков", description = "CRUD-операции и другие эндпоинты для работы с поставками носков")
 public class StorageController {
 
-    StorageService storageService;
-
-    public StorageController(StorageService storageService) {
-        this.storageService = storageService;
-    }
+    private final StorageService storageService;
 
     @PostMapping()
+    @Operation (summary = "добавление носков на склад")
     public ResponseEntity<Socks[]> addSocksInStorage(@Valid @RequestBody Socks...socks) throws QuantityException {
         for (Socks sock : socks) {
             storageService.addSocksInStorage(sock);
         }
         return ResponseEntity.ok(socks);
     }
-
-    @GetMapping("/allSocks")
-    public ResponseEntity<List<Socks>> getAllSocksList() {
-        return ResponseEntity.ok(storageService.obtainAllSocks());
+    @PutMapping
+    @Operation (summary = "Отгрузка со склада")
+    public ResponseEntity<Object> getFromStock(@Valid @RequestBody Socks socks) {
+        int a = 0;
+        try {
+            a = storageService.getFromStock(socks);
+        } catch (QuantityException e) {
+            ResponseEntity.badRequest().body(e);
+        }
+        return ResponseEntity.ok().body("На складе осталось " + a);
     }
 
-    @GetMapping("/quantity")
+    @GetMapping
+    @Operation(summary = "Остаток носков на складе")
 
     public ResponseEntity<Integer> obtainQuantity(Color color,
                                                   Size size,
@@ -44,18 +53,8 @@ public class StorageController {
         return ResponseEntity.ok(quantity);
     }
 
-    @PutMapping
-    public ResponseEntity<Object> getFromStock(@Valid @RequestBody Socks socks) {
-        int a = 0;
-        try {
-            a = storageService.getFromStock(socks);
-        } catch (QuantityException e) {
-            ResponseEntity.badRequest().body(e);
-        }
-        return ResponseEntity.ok().body("На складе осталось " + a);
-    }
-
     @DeleteMapping
+    @Operation(summary = "Списание бракованных носков")
     public ResponseEntity<Object> deleteFromStock(@Valid @RequestBody Socks socks) {
         int a = 0;
         try {
@@ -64,5 +63,10 @@ public class StorageController {
             ResponseEntity.badRequest().body(e);
         }
         return ResponseEntity.ok().body("На складе осталось " + a);
+    }
+    @GetMapping("/allSocks")
+    @Operation(summary = "Получение всех видов носков", description = "Входные данные не нужны")
+    public ResponseEntity<List<Socks>> getAllSocksList() {
+        return ResponseEntity.ok(storageService.obtainAllSocks());
     }
 }
